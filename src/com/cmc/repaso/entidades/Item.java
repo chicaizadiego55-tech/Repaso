0307547a43d2da53f18ae813b895a37c0e107f0a
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-08-05 17:05:55 -0500

    Ejercicio 4 implementado, taller finalizado

--- src/com/cmc/repaso/entidades/Item.java
+++ src/com/cmc/repaso/entidades/Item.java
@@ -2,4 +2,34 @@ package com.cmc.repaso.entidades;
 
 public class Item {
 
+    public String nombre;
+    public int productosActuales;
+    public int productosDevueltos;
+    public int productosVendidos;
+
+    public void imprimir() {
+
+        System.out.println("Producto: " + nombre);
+        System.out.println("Actuales: " + productosActuales);
+        System.out.println("Vendidos: " + productosVendidos);
+        System.out.println("Devueltos: " + productosDevueltos);
+        System.out.println();
+
+    }
+
+    public void vender(int cantidad) {
+
+        productosActuales = productosActuales - cantidad;
+        productosVendidos = productosVendidos + cantidad;
+
+    }
+
+    public void devolver(int cantidad) {
+
+        productosActuales = productosActuales + cantidad;
+        productosVendidos = productosVendidos - cantidad;
+        productosDevueltos = productosDevueltos + cantidad;
+
+    }
+
 }
--- src/com/cmc/repaso/test/TestItem.java
+++ src/com/cmc/repaso/test/TestItem.java
@@ -1,5 +1,37 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Item;
+
 public class TestItem {
+
+    public static void main(String[] args) {
+
+        Item item1 = new Item();
+
+        item1.nombre = "Coca Cola";
+        item1.productosActuales = 20;
+
+        item1.imprimir();
+
+        item1.vender(5);
+        item1.imprimir();
+
+        item1.devolver(2);
+        item1.imprimir();
+
+        Item item2 = new Item();
+
+        item2.nombre = "Galletas";
+        item2.productosActuales = 20;
+
+        item2.imprimir();
+
+        item2.vender(8);
+        item2.imprimir();
+
+        item2.devolver(3);
+        item2.imprimir();
+
+    }
 
 }
