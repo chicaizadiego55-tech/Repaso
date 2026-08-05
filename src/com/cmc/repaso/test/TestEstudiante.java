69d5dc9f74d28af8192feaf965026c8444ea4de6
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-08-05 16:47:52 -0500

    Ejercicio 2 implementado

--- src/com/cmc/repaso/test/TestEstudiante.java
+++ src/com/cmc/repaso/test/TestEstudiante.java
@@ -1,5 +1,19 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Estudiante;
+
 public class TestEstudiante {
+
+    public static void main(String[] args) {
+
+        Estudiante e1 = new Estudiante("Diego");
+
+        e1.calificar(9);
+
+        System.out.println("Nombre: " + e1.nombre);
+        System.out.println("Nota: " + e1.nota);
+        System.out.println("Resultado: " + e1.resultado);
+
+    }
 
 }
--- src/com/cmc/repaso/test/TestValidacion.java
+++ src/com/cmc/repaso/test/TestValidacion.java
@@ -1,5 +1,16 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Validacion;
+
 public class TestValidacion {
+
+    public static void main(String[] args) {
+
+        Validacion v = new Validacion();
+
+        System.out.println(v.validarMonto(100));
+        System.out.println(v.validarMonto(-50));
+
+    }
 
 }
