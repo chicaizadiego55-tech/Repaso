d86001ffb12490a144ba165959b8e8f8f05ea6f9
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-08-05 16:59:20 -0500

    Ejercicio 3 implementado

--- src/com/cmc/repaso/entidades/Producto.java
+++ src/com/cmc/repaso/entidades/Producto.java
@@ -2,4 +2,30 @@ package com.cmc.repaso.entidades;
 
 public class Producto {
 
+    public String nombre;
+    public double precio;
+
+    public Producto(String nombre, double precio) {
+
+        this.nombre = nombre;
+        this.precio = precio;
+
+    }
+
+    public void setPrecio(double precio) {
+
+        if (precio < 0) {
+            this.precio = precio * -1;
+        } else {
+            this.precio = precio;
+        }
+
+    }
+
+    public double calcularPrecioPromo(double descuento) {
+
+        return precio - (precio * descuento / 100);
+
+    }
+
 }
--- src/com/cmc/repaso/test/TestProducto.java
+++ src/com/cmc/repaso/test/TestProducto.java
@@ -1,5 +1,19 @@
 package com.cmc.repaso.test;
 
+import com.cmc.repaso.entidades.Producto;
+
 public class TestProducto {
+
+    public static void main(String[] args) {
+
+        Producto p = new Producto("Arroz", 20);
+
+        p.setPrecio(-30);
+
+        System.out.println("Producto: " + p.nombre);
+        System.out.println("Precio: " + p.precio);
+        System.out.println("Precio con descuento: " + p.calcularPrecioPromo(10));
+
+    }
 
 }
